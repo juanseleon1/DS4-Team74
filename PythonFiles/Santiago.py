f842f0884868c8462799fe224b7d353278c62096
shernandezoa <shernandezoa@gmail.com>
2021-06-05 11:40:34 -0500

    testing github
    minor changes

--- PythonFiles/Santiago.py
+++ PythonFiles/Santiago.py
@@ -3,7 +3,7 @@ import scipy
 import numpy
 import seaborn
 import matplotlib
-import folium
+# import folium
 
 # This is a sample Python script.
 
@@ -13,11 +13,11 @@ import folium
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, this is {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Santiago')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
